eef8aea5af68ffe2b378edcbfb427aebfd86cfaa
Jasper Borcena <jasper.v.borcena@gmail.com>
2026-07-09 20:11:21 +0800

    Add Firebase config to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: ''
+    apiKey: "AIzaSyAXuT1THCssURAGTXBttcWkp1vrFSKsqes",
+    authDomain: "tovrikacms.firebaseapp.com",
+    projectId: "tovrikacms",
+    storageBucket: "tovrikacms.firebasestorage.app",
+    messagingSenderId: "721304283865",
+    appId: "1:721304283865:web:f6de53b6637a4f7022acc8",
+    measurementId: "G-2TD6P6HNYF"
   }
 };
